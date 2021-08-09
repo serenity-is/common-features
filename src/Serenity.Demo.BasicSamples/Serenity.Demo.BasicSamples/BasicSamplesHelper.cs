454e94e6e3dee40563ea882b674b71c583920b1c
Furkan Evran <furkan.evran@serenity.is>
2021-08-09 14:41:46 +0300

    fix broken urls on docs

--- src/Serenity.Demo.BasicSamples/Serenity.Demo.BasicSamples/BasicSamplesHelper.cs
+++ src/Serenity.Demo.BasicSamples/Serenity.Demo.BasicSamples/BasicSamplesHelper.cs
@@ -10,21 +10,35 @@ namespace Serenity.Demo.BasicSamples
     {
         public static HtmlString BasicSamplesSourceFile(this HtmlHelper helper, string file)
         {
-            var viewLocation = ((RazorView)helper.ViewContext.View).Path;
-            var absolutePath = Path.GetDirectoryName(viewLocation).Replace('\\', '/') + '/';
-            var relative = file.Replace('\\', '/');
-            var question = relative.IndexOf('?', StringComparison.Ordinal);
-            if (question >= 0)
+            if (file == null || file.Length == 0)
+                return null;
+
+            string path;
+
+            if (file.StartsWith("/"))
             {
-                relative = new Uri("x:" + absolutePath + relative.Substring(0, question))
-                    .AbsolutePath.Substring(2) + relative.Substring(question);
+                path = file.Substring(1);
             }
             else
-                relative = new Uri("x:" + absolutePath + relative).AbsolutePath.Substring(2);
+            {
+                var viewLocation = ((RazorView)helper.ViewContext.View).Path;
+                var absolutePath = Path.GetDirectoryName(viewLocation).Replace('\\', '/') + '/';
+                var relative = file.Replace('\\', '/');
+                var question = relative.IndexOf('?', StringComparison.Ordinal);
+                if (question >= 0)
+                {
+                    relative = new Uri("x:" + absolutePath + relative.Substring(0, question))
+                        .AbsolutePath.Substring(2) + relative.Substring(question);
+                }
+                else
+                    relative = new Uri("x:" + absolutePath + relative).AbsolutePath.Substring(2);
+
+                path = "Serenity.Demo.BasicSamples" + relative;
+            }
 
             return new HtmlString("<a target=\"blank\" style=\"font-weight: bold; color: #ffc\" href=\"" +
                 helper.Encode("https://github.com/serenity-is/common-features/" +
-                    "blob/master/src/Serenity.Demo.BasicSamples/" + relative) +
+                    "blob/master/src/" + path) +
                 "\">" + helper.Encode(Path.GetFileName(file)) + "</a>");
         }
     }

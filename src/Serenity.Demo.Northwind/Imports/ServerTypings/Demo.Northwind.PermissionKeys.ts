45a5f9bd8e627770ef3f0f2dfaa2ef9fba234a1a
volkanceylan <volkanceylan@gmail.com>
2022-10-22 20:55:13 +0300

    :up: 6.2.9

--- src/Serenity.Demo.Northwind/Imports/ServerTypings/Demo.Northwind.PermissionKeys.ts
+++ src/Serenity.Demo.Northwind/Imports/ServerTypings/Demo.Northwind.PermissionKeys.ts
@@ -2,7 +2,7 @@
     export namespace PermissionKeys {
         export const General = "Northwind:General";
 
-        namespace Customer {
+        export namespace Customer {
             export const Delete = "Northwind:Customer:Delete";
             export const Modify = "Northwind:Customer:Modify";
             export const View = "Northwind:Customer:View";

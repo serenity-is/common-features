1127a7e9460a8ac0a3e3d87e24f65d4b61cf77ad
volkanceylan <volkanceylan@gmail.com>
2022-12-07 14:39:19 +0300

    update https

--- src/Serenity.Demo.BasicSamples/Modules/Dialogs/DialogBoxes/DialogBoxes.ts
+++ src/Serenity.Demo.BasicSamples/Modules/Dialogs/DialogBoxes/DialogBoxes.ts
@@ -89,7 +89,7 @@ function alertWithHtmlContent() {
     $('#AlertWithHtmlContent').click(() => {
         alert("<h4>Here is some HTML content!</h4>" +
             "<ul><li>Item 1</li><li>Item 2</li >" +
-            "<li>Visit <a href='http://serenity.is/' target='_blank' style='color: #ddf'>http://serenity.is/</a>!</li></ul>", {
+            "<li>Visit <a href='https://serenity.is/' target='_blank' style='color: #ddf'>https://serenity.is/</a>!</li></ul>", {
             htmlEncode: false
         });
     });

f2aadc2d9ea9a4510010e7354a1eb20cb25fb9cf
volkanceylan <volkanceylan@gmail.com>
2023-03-30 23:06:52 +0300

    async sample for EmployeeListFormatter

--- src/Serenity.Demo.Northwind/Modules/Customer/EmployeeListFormatter.ts
+++ src/Serenity.Demo.Northwind/Modules/Customer/EmployeeListFormatter.ts
@@ -1,16 +1,37 @@
 import { Decorators, Formatter } from "@serenity-is/corelib";
+import { Lookup } from "@serenity-is/corelib/q";
 import { FormatterContext } from "@serenity-is/sleekgrid";
 import { EmployeeRow } from "../ServerTypes/Demo";
 
+var employeeLookup: Lookup<EmployeeRow>;
+var employeePromise: Promise<any>;
+
 @Decorators.registerFormatter('Serenity.Demo.Northwind.EmployeeListFormatter')
 export class EmployeeListFormatter implements Formatter {
+
     format(ctx: FormatterContext) {
+
         var idList = ctx.value as string[];
         if (!idList || !idList.length)
             return "";
 
-        var byId = EmployeeRow.getLookup().itemById;
-        let z: EmployeeRow;
-        return idList.map(x => ((z = byId[x]) ? z.FullName : x)).join(", ");
+        
+        if (!employeeLookup) {
+
+            if (!employeePromise) {
+                employeePromise = EmployeeRow.getLookupAsync().then(lookup => {
+                    employeeLookup = lookup;
+                    ctx.grid?.invalidate();
+                }).catch(() => employeePromise = null);
+            }
+
+            return `<i class="fa fa-spinner"></i>`;
+        }
+
+        var byId = employeeLookup.itemById;
+        return idList.map(x => {
+            var z = byId[x];
+            return z == null ? x : z.FullName;
+        }).join(", ");
     }
 }

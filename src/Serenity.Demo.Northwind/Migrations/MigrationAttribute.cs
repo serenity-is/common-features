90ca05904c3a099e21f0c502c3f6e3a4b6f85e11
volkanceylan <volkanceylan@gmail.com>
2023-10-17 18:00:48 +0300

    add a MigrationAttributeBase class that can be reused by feature packages to define their specific MigrationAttribute classes

--- src/Serenity.Demo.Northwind/Migrations/MigrationAttribute.cs
+++ src/Serenity.Demo.Northwind/Migrations/MigrationAttribute.cs
@@ -1,12 +1,8 @@
-﻿namespace Serenity.Demo.Northwind.Migrations;
+namespace Serenity.Demo.Northwind.Migrations;
 
-public class MigrationAttribute : FluentMigrator.MigrationAttribute
+public class MigrationAttribute : Extensions.MigrationAttributeBase
 {
-    public MigrationAttribute(long version)
-        : base((version >= 20010101_0000 && version <= 99990101_0000) ? version * 100 : version)
+    public MigrationAttribute(long version) : base(version)
     {
-        if (Version < 20010101_000000 || Version > 99990101_000000)
-            throw new Exception("Migration versions must be in yyyyMMdd_HHmm or " +
-                "yyyyMMdd_HHmm_ss format! Version " + version + " is incorrect.");
     }
 }

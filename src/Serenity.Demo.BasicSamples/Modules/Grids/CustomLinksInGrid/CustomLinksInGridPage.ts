839766ddb1b9f4f9cf5088ab590a2af9b0249d20
volkanceylan <volkanceylan@gmail.com>
2024-08-10 11:40:40 +0300

    Use asHtml

--- src/Serenity.Demo.BasicSamples/Modules/Grids/CustomLinksInGrid/CustomLinksInGridPage.ts
+++ src/Serenity.Demo.BasicSamples/Modules/Grids/CustomLinksInGrid/CustomLinksInGridPage.ts
@@ -15,16 +15,16 @@ export class CustomLinksInGrid extends OrderGrid {
         var columns = new OrderColumns(super.getColumns());
 
         columns.CustomerCompanyName && (columns.CustomerCompanyName.format =
-            ctx => `<a href="javascript:;" class="customer-link">${ctx.escape()}</a>`);
+            ctx => ctx.asHtml(`<a href="#" class="customer-link">${ctx.escape()}</a>`));
 
         columns.OrderDate && (columns.OrderDate.format =
-            ctx => `<a href="javascript:;" class="date-link">${ctx.escape(formatDate(ctx.value))}</a>`);
+            ctx => ctx.asHtml(`<a href="#" class="date-link">${ctx.escape(formatDate(ctx.value))}</a>`));
 
         columns.EmployeeFullName && (columns.EmployeeFullName.format =
-            ctx => `<a href="javascript:;" class="employee-link">${ctx.escape()}</a>`);
+            ctx => ctx.asHtml(`<a href="#" class="employee-link">${ctx.escape()}</a>`));
 
         columns.ShipCountry && (columns.ShipCountry.format =
-            ctx => `<a href="javascript:;" class="ship-country-link">${ctx.escape()}</a>`);
+            ctx => ctx.asHtml(`<a href="#" class="ship-country-link">${ctx.escape()}</a>`));
 
         return columns.valueOf();
     }
--- src/Serenity.Extensions/Modules/Formatters/SingleLineTextFormatter.ts
+++ src/Serenity.Extensions/Modules/Formatters/SingleLineTextFormatter.ts
@@ -4,7 +4,7 @@ import { FormatterContext } from "@serenity-is/sleekgrid";
 @Decorators.registerFormatter('Serenity.Extensions.SingleLineTextFormatter')
 export class SingleLineTextFormatter implements Formatter {
     format(ctx: FormatterContext) {
-        return ctx.asText(SingleLineTextFormatter.formatValue(ctx.value));
+        return ctx.escape(SingleLineTextFormatter.formatValue(ctx.value));
     }
 
     /** Formats html value as text, note that return value is plain text, e.g. is not html escaped */

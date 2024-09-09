1e2f04fd87dedd97566c0bfeef20afabcd5d15ec
volkanceylan <volkanceylan@gmail.com>
2024-09-09 19:14:47 +0300

    Generated ESM helpers via sergen MVC command

---
+++ src/Serenity.Demo.Northwind/Imports/MVC/ESM.cs
@@ -0,0 +1,12 @@
+﻿namespace Serenity.Demo.Northwind;
+
+public static partial class ESM
+{
+    public static partial class Modules
+    {
+        public static partial class Category
+        {
+            public const string CategoryPage = "~/Serenity.Demo.Northwind/esm/Modules/Category/CategoryPage.js";
+        }
+    }
+}

3a6c7ba32513fb8ae67976371cb222d9c26fc5db
volkanceylan <volkanceylan@gmail.com>
2024-10-13 22:16:28 +0300

    Add typeNumber test utility function

--- build/test-utils/editorutils.ts
+++ build/test-utils/editorutils.ts
@@ -6,3 +6,10 @@ export function typeText(editor: { value: string, element: Fluent }, value: stri
     editor.element.trigger("blur");
     editor.element.trigger("change");
 }
+
+export function typeNumber(editor: { value: number, element: Fluent }, value: number) {
+    editor.element.trigger("focus");
+    editor.value = value;
+    editor.element.trigger("blur");
+    editor.element.trigger("change");
+}
